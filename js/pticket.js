16a5198b20d977d255cc5b76ddf641673d531d6a
Hasan-Mahmud <hasanmiru@gmail.com>
2025-11-02 18:52:22 +0600

    footer added and js add in buy Ticket button

---
+++ js/pticket.js
@@ -0,0 +1,6 @@
+const buyTicket = document.getElementById("buyTickets");
+const pahribahan = document.getElementById("paribahan");
+
+buyTicket.addEventListener("click", function(){
+    pahribahan.scrollIntoView({behavior: "smooth"});
+});
